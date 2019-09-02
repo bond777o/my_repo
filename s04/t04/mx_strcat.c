d2decf252826ba0024c1f0675fd5366bb47a95b2
bond777o <bondarenko.oleksanda345@gmail.com>
2019-09-02 17:50:26 +0300

    t04

--- s04/t04/mx_strcat.c
+++ s04/t04/mx_strcat.c
@@ -2,16 +2,14 @@
 #include <unistd.h>
 #include "../../my_libs/mx_strlen.c"
 
-char *mx_strcat(char *s1, char *s2)
+char *mx_strcat(char *s1, const char *s2)
 {
     int len_s2 = mx_strlen(s2);
-    printf("len s2 = %d\n", len_s2);
     int len_s1 = mx_strlen(s1) + len_s2;
-    printf("len s1 = %d\n", len_s1);
 
-    int i;
+    int i = mx_strlen(s1);
     int j;
-    for (j = 0; i < len_s1; i++)
+    for (j = 0; i <= len_s1; i++, j++)
     {
         s1[i] = s2[j];
     }
@@ -20,14 +18,11 @@ char *mx_strcat(char *s1, char *s2)
 
 int main()
 {
-    char ss1[4] = "qwer";
-    printf("%s\n", ss1);
+    char ss1[80] = "qwer";
 
-    char ss2[4] = "qwer";
-    printf("%s\n", ss2);
+    char ss2[] = "rewq";
     
-    //mx_strcat(ss1, ss2);
-    printf("%s\n", ss1);
+    mx_strcat(ss1, ss2);
     printf("%s\n", ss1);
     return 0;
 }
--- s04/t04/testt.c
+++ s04/t04/testt.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-    const char ss1[4] = "qwer";
+    const char ss1[] = "qwer";
     printf("ss1 = %s\n", ss1);
 
-    const char ss2[4] = "abcd";
+    const char ss2[] = "abcd";
     printf("ss2 = %s\n", ss2);
+
+    const char ss3[] = "zxcv";
+    printf("ss3 = %s\n", ss3);
     
-    printf("%s\n", ss1);
+    printf("%s\n", ss2);
     return 0;
 }
